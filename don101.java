5b07ee70e27653cb24106caa4ac11145b363875d
bangurad <bangurad@vcu.edu>
2025-01-18 19:16:59 -0500

    Create don101.java

---
+++ don101.java
@@ -0,0 +1,52 @@
+//Class Header//
+public class don101{    //Start Class//
+
+    public static void main(String args[]){ //start main method//
+
+        System.out.println("hello,world");
+
+        // sing line comment
+
+        /*
+         * 
+         * Multi line comments
+         * 
+         */
+
+         /*
+          * Varaible types
+          int (stands for integer)- these are whole number data types (for example 1,2,3)
+          * double (stands for double)- these are for decimal data types for example.(0.5, 1.2)
+          * String - these are words, quotes, letters, etc. (for example "Chicken", "W", "I")
+          * boolean - these return true or false values (for example isHot = false;)
+          * short - these are for whole numbers also but only from numbers (-32,768 to 32,767)
+          * long - these are whole numbers but they store all whole numbers in exisitence (infinite)
+          * char- (short for character) - this used to store and read characters (for examples "d", "s". 'k')
+          * 
+          */
+
+         // age variable
+         int age = 19;
+
+         // name variable
+         String name = "Rick";
+
+         // temperature variable
+         double temp = 98.3;
+
+         // hashtag variable
+         char hash = '#';
+
+         // display name
+         System.out.println("I am " + age + " years old ");
+
+         // display our name and hastag
+         System.out.println("My name is " + hash + name);
+
+         // practice
+         System.out.println("The temperature today is " + temp);
+
+
+    }   //end main method
+
+}   //End Class
